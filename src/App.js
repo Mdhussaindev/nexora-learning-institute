8206fb538e10a25d1c12a2d0cc1f251e901aa85e
Mdhussaindev <mdhussain.dev00@gmail.com>
2026-08-17 01:33:21 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -8,10 +9,14 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/" element={<Home />} />
+
         <Route path="/about" element={<div>About Page</div>} />
         <Route path="/courses" element={<div>Courses Page</div>} />
-        <Route path="/instructors" element={<div>Instructors Page</div>} />
+        <Route
+          path="/instructors"
+          element={<div>Instructors Page</div>}
+        />
         <Route path="/contact" element={<div>Contact Page</div>} />
       </Routes>
     </BrowserRouter>
